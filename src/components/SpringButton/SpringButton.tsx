5a8987abb6757bee2ca5dfe20264c5cc21243af3
RogueManiac <70204504+RogueManiac@users.noreply.github.com>
2023-09-10 20:17:55 +0300

    SpringButton iter.0b

---
+++ src/components/SpringButton/SpringButton.tsx
@@ -0,0 +1,15 @@
+import { Button, ButtonProps } from "react-native-paper";
+import Animated from 'react-native-reanimated';
+
+
+/**
+ * Button that springs on click
+ * @constructor
+ */
+const SpringButton = (props: ButtonProps) => {
+
+
+  return <Button {...props} />
+};
+
+export default SpringButton;

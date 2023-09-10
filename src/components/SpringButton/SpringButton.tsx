5566fa6aef0c16c7e746924d7dff42d0a7237972
RogueManiac <70204504+RogueManiac@users.noreply.github.com>
2023-09-10 21:05:53 +0300

    Implemented SpringButton

--- src/components/SpringButton/SpringButton.tsx
+++ src/components/SpringButton/SpringButton.tsx
@@ -1,15 +1,42 @@
-import { Button, ButtonProps } from "react-native-paper";
-import Animated from 'react-native-reanimated';
-
+import React from "react";
+import Animated, {
+  withSpring,
+  useSharedValue,
+  useAnimatedStyle,
+  withSequence,
+  withDelay, WithSpringConfig
+} from "react-native-reanimated";
+import { ButtonProps, Button } from "react-native-paper";
 
 /**
- * Button that springs on click
+ * RN-Paper Button that springs on click
+ *
+ * @param props RN-Paper Button properties
  * @constructor
  */
 const SpringButton = (props: ButtonProps) => {
+  const scaleValue = useSharedValue(1);
+  const springConfig: WithSpringConfig = {
+  };
 
+  // Animated style for the button
+  const animationStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scaleValue.value }],
+    };
+  });
 
-  return <Button {...props} />
+  // Handler for expanding and collapsing
+  const handlePress = () => {
+    scaleValue.value = withSequence(withSpring(1.1, springConfig),
+      withDelay(30, withSpring(1, springConfig)));
+  };
+
+  return (
+      <Animated.View style={[animationStyle]}>
+        <Button {...props} onPress={handlePress} />
+      </Animated.View>
+  );
 };
 
 export default SpringButton;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as Brand } from './Brand/Brand';
 export { default as HomeCarousel } from './HomeCarousel/HomeCarousel';
 export { default as PaginationBar } from './PaginationBar/PaginationBar';
 export { default as CacheImage } from './CacheImage/CacheImage';
+export { default as SpringButton } from './SpringButton/SpringButton';
 export type { CarouselProps } from './HomeCarousel/HomeCarousel';

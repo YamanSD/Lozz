467e2c0eebcd54e041fb09bf66cdafdca1137143
RogueManiac <70204504+RogueManiac@users.noreply.github.com>
2023-09-22 23:26:15 +0300

    Testing DynamicForms in Formic

---
+++ src/screens/Products/CreationScreen/DynamicInputForm.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const DynamicInputForm = () => {
+  const initialValues = {
+    dynamicFields: [{ title: '', description: '' }], // Initialize with an empty item
+  };
+
+  const validationSchema = Yup.object().shape({
+    dynamicFields: Yup.array().of(
+      Yup.object().shape({
+        title: Yup.string().required('Title is required'),
+        description: Yup.string().required('Description is required'),
+      })
+    ),
+  });
+
+  const handleSubmit = (values) => {
+    // Handle form submission here
+    console.log('Submitted Values:', values.dynamicFields);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ values, setFieldValue, handleSubmit, errors }) => {
+        return (
+        <View>
+          {values.dynamicFields.map((item, index) => (
+            <View key={index}>
+              <TextInput
+                onChangeText={(text) => {
+                  const updatedFields = [...values.dynamicFields];
+                  updatedFields[index].title = text;
+                  setFieldValue('dynamicFields', updatedFields);
+                }}
+                value={item.title}
+                placeholder={`Title ${index + 1}`}
+              />
+              <TextInput
+                onChangeText={(text) => {
+                  const updatedFields = [...values.dynamicFields];
+                  updatedFields[index].description = text;
+                  setFieldValue('dynamicFields', updatedFields);
+                }}
+                value={item.description}
+                placeholder={`Description ${index + 1}`}
+              />
+              <Text style={{ color: 'red' }}>
+                {errors.dynamicFields && errors.dynamicFields[index]?.title}
+              </Text>
+              <Text style={{ color: 'red' }}>
+                {errors.dynamicFields && errors.dynamicFields[index]?.description}
+              </Text>
+            </View>
+          ))}
+          <Button
+            title="Add Item"
+            onPress={() => {
+              if (values.dynamicFields.length < 10) {
+                const updatedFields = [...values.dynamicFields, { title: '', description: '' }];
+                setFieldValue('dynamicFields', updatedFields);
+              }
+            }} // Limit to a reasonable number of items
+          />
+          <Button
+            title="Remove Item"
+            onPress={() => {
+              if (values.dynamicFields.length > 1) {
+                const updatedFields = [...values.dynamicFields];
+                updatedFields.pop(); // Remove the last item
+                setFieldValue('dynamicFields', updatedFields);
+              }
+            }}
+          />
+          <Button title="Submit" onPress={handleSubmit} />
+        </View>
+      )}}
+    </Formik>
+  );
+};
+
+export default DynamicInputForm;
